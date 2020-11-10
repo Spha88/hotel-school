d6897e408073ef3603051c3f158e20bac0d196bb
SIPHAMANDLA MEHLOMAKULU <mr.smehlomakulu@gmail.com>
2020-11-10 16:13:56 +0200

    Add line-height to the list in about page

--- public/form_handler.php
+++ public/form_handler.php
@@ -133,7 +133,7 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST' ){
 
     //sending the email
         if ($OK == true) {
-          $to = "mr.smehlomakulu@gmail.com";
+          $to = "info@shprevenance.co.za";
           $emailSubject = "Online query: ".$subject;
           $body = "Name of Client: ".$name."\nContact Number: ".$number."\n\nMessage: \n".$message;
           $headers =  "From: $name <$email>"; 
