dabb744610cf8697ea728662fa4e91ee3cac8bf8
SIPHAMANDLA MEHLOMAKULU <mr.smehlomakulu@gmail.com>
2020-11-10 05:33:02 +0200

    Add animations to the about page

--- components/AboutContent/AboutContent.js
+++ components/AboutContent/AboutContent.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Zoom from 'react-reveal/Zoom';
 
 import styles from './AboutContent.module.scss';
 
@@ -9,37 +10,53 @@ const AboutContent = () => {
                 <h2>WE ARE PRÉVENANCE HOTEL SCHOOL</h2>
             </header>
             <main>
-                <img src="/images/about1.jpeg" alt="wine and wine glasses" />
+                <Zoom><img src="/images/about1.jpeg" alt="wine and wine glasses" /></Zoom>
                 <div className={styles.AboutContent}>
                     <div className={styles.Heading}>
                         <div className={styles.HeadingInner}>
-                            <h2>OUR STORY</h2>
-                            <div className={styles.Line}></div>
+                            <Zoom><h2>OUR STORY</h2></Zoom>
+                            <Zoom delay={500}><div className={styles.Line}></div></Zoom>
                         </div>
                     </div>
                     <div className={styles.Paragraph}>
-                        <p>
-                            Prévenance Hotel School <strong>CATHSETA accredited</strong> is a professional academy focused on training students who intend to establish themselves as leaders in the hospitality industry.
-                        </p>
-                        <p>
-                            Our students have a range of excellent courses to choose from. Through our intake program we help them find just the right course and career for their personality.
-                        </p>
-                        <p>
-                            Have practical fun while learning, join us for best journey of your life!
-                        </p>
+                        <Zoom>
+                            <p>
+                                Prévenance Hotel School <strong>CATHSETA accredited</strong> is a professional academy focused on training students who intend to establish themselves as leaders in the hospitality industry.
+                            </p>
+                        </Zoom>
+                        <Zoom>
+                            <p>
+                                Our students have a range of excellent courses to choose from. Through our intake program we help them find just the right course and career for their personality.
+                    </p>
+                        </Zoom>
+                        <Zoom>
+                            <p>
+                                Have practical fun while learning, join us for best journey of your life!
+                    </p>
+                        </Zoom>
 
-                        <h2>Our history</h2>
+                        <Zoom>
+                            <h2>Our history</h2>
+                        </Zoom>
 
-                        <p>We started Prévenance Hotel School as a result of our passion for excellence in service.
-                        Having been in the hospitality space for a number of years, it became clear that gaps and areas of improvement exist in our industry. We soon realised that no amount screaming, hiring and firing will improve the service offering by a number of hospitality establishments, from guest houses to hotels.
+                        <Zoom>
+                            <p>We started Prévenance Hotel School as a result of our passion for excellence in service.
+                            Having been in the hospitality space for a number of years, it became clear that gaps and areas of improvement exist in our industry. We soon realised that no amount screaming, hiring and firing will improve the service offering by a number of hospitality establishments, from guest houses to hotels.
                         </p>
+                        </Zoom>
 
-                        <p>Training of staff to take pride in what they do, became the only option. At the center of our curriculum is the intention to produce graduates that not only know what they do, but also take pride in their service and desire to only give their best.</p>
+                        <Zoom>
+                            <p>Training of staff to take pride in what they do, became the only option. At the center of our curriculum is the intention to produce graduates that not only know what they do, but also take pride in their service and desire to only give their best.</p>
+                        </Zoom>
 
-                        <p>We have no doubt of the great impact our academy will make in the lives and careers of many young people across our country and the continent.</p>
+                        <Zoom>
+                            <p>We have no doubt of the great impact our academy will make in the lives and careers of many young people across our country and the continent.</p>
+                        </Zoom>
                     </div>
                 </div>
-                <img src="/images/about2.jpeg" alt="writing and studying" />
+                <Zoom>
+                    <img src="/images/about2.jpeg" alt="writing and studying" />
+                </Zoom>
             </main>
         </section>
     )
