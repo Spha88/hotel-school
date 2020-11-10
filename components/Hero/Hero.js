3137a979c555a016593161e731aa8ce14e19388e
SIPHAMANDLA MEHLOMAKULU <mr.smehlomakulu@gmail.com>
2020-11-10 06:09:46 +0200

    Add animations to Hero component

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,24 +1,35 @@
 import React from 'react'
-import styles from './Hero.module.scss'
 import Link from 'next/link';
+import Zoom from 'react-reveal/Zoom';
+import Flip from 'react-reveal/Flip';
+import Bounce from 'react-reveal/Bounce';
+
+import styles from './Hero.module.scss'
+
 
 const Hero = () => {
     return (
         <div className={styles.Hero}>
             <div className={styles.HeroContent}>
                 <div className={styles.Logo}>
-                    <img src="/images/logo_square_blue_bg_white.png" alt="logo" />
-                    <h1>Invest in yourself</h1>
+                    <Flip left>
+                        <img src="/images/logo_square_blue_bg_white.png" alt="logo" />
+                    </Flip>
+                    <Zoom>
+                        <h1>Invest in yourself</h1>
+                    </Zoom>
                 </div>
                 <div className={styles.CallToAction}>
-                    <Link href="/contact">
-                        <a>
-                            Enrol Now
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
-                            </svg>
-                        </a>
-                    </Link>
+                    <Bounce right delay={1000}>
+                        <Link href="/contact">
+                            <a>
+                                Enrol Now
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+                                </svg>
+                            </a>
+                        </Link>
+                    </Bounce>
                 </div>
             </div>
             <div className={styles.HeroBackground} style={{ backgroundImage: "url(/images/bg2.jpeg)" }}></div>
