ff716407b7ea4ea11dd78fff4ceeae347d1446cc
SIPHAMANDLA MEHLOMAKULU <mr.smehlomakulu@gmail.com>
2020-11-10 05:48:37 +0200

    Changed burger icon to svg

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -82,7 +82,9 @@ const Header = () => {
                 </ul>
                 <div className={styles.BurgerIcon}>
                     <button onClick={toggleMobileNav}>
-                        <i className="material-icons">menu</i>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
+                        </svg>
                     </button>
                 </div>
             </div>
